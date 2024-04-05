b2fb23390a02338d41a4904d76d4e434c9701f16
praghur <166060957+praghur@users.noreply.github.com>
2024-04-05 15:31:43 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,11 @@
 NODE_ID=$(geni-get client_id)
 
 if [ $NODE_ID = "rue1" ]; then
-    /local/repository/start-ue.sh
+    /local/repository/start-ue1.sh
+if [ $NODE_ID = "rue2" ]; then
+    /local/repository/start-ue2.sh
+if [ $NODE_ID = "rue3" ]; then
+    /local/repository/start-ue3.sh
 elif [ $NODE_ID = "enb1" ]; then
     /local/repository/start-enb1.sh
 elif [ $NODE_ID = "enb2" ]; then
