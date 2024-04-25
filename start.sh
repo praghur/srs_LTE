76a6b5daa6d10a64363d16bb14e5cc4f5c549e84
praghur <166060957+praghur@users.noreply.github.com>
2024-04-25 13:37:53 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,23 +1,19 @@
-#!/usr/bin/env bash
-
-# Run appropriate setup script
-
 NODE_ID=$(geni-get client_id)
 
-if [ $NODE_ID = "rue1" ]; then
+if [ "$NODE_ID" = "rue1" ]; then
     /local/repository/start-ue1.sh
-elif [ $NODE_ID = "rue2" ]; then
+elif [ "$NODE_ID" = "rue2" ]; then
     /local/repository/start-ue2.sh
-elif [ $NODE_ID = "rue3" ]; then
+elif [ "$NODE_ID" = "rue3" ]; then
     /local/repository/start-ue3.sh
-elif [ $NODE_ID = "enb1" ]; then
+elif [ "$NODE_ID" = "enb1" ]; then
     /local/repository/start-enb1.sh
-elif [ $NODE_ID = "enb2" ]; then
+elif [ "$NODE_ID" = "enb2" ]; then
     /local/repository/start-enb2.sh
-elif [ $NODE_ID = "enbr" ]; then
+elif [ "$NODE_ID" = "enbr" ]; then
     /local/repository/start-enbr.sh
-elif [ $NODE_ID = "epc" ]; then
+elif [ "$NODE_ID" = "epc" ]; then
     /local/repository/start-epc.sh
 else
-    echo "no setup necessary"
+    echo "No setup necessary"
 fi
